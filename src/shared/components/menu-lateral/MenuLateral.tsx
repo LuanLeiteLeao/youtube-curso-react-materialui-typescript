5eaca178ae34d58131688d039c8feb2e83d5ecfa
Luan Leite Leão <leaoluanleite@gmail.com>
2024-07-20 18:53:42 -0300

    feat: criando menu lateral

---
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -0,0 +1,52 @@
+
+import { Avatar, Box, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useTheme } from "@mui/material"
+
+interface IPorps {
+    children: React.ReactNode
+}
+
+export const MenuLateral: React.FC<IPorps> = ({ children }) => {
+    const theme = useTheme()
+
+    function avatarBox() {
+        return (
+            <Box width="100%" height={theme.spacing(20)} display="flex" alignItems="center" justifyContent="center">
+                <Avatar
+                    sx={{ height: theme.spacing(12), width: theme.spacing(12) }}
+                    src="https://yt3.ggpht.com/grfYgQadT8iNg9WPb-jkrKB-9224y_DBDXAOtV4Yt7cyQmtR47J_453uveQOTDsp_dRSH851TMM=s108-c-k-c0x00ffffff-no-rj" />
+            </Box>
+        )
+    }
+
+    return (
+        <>
+            <Drawer variant="permanent">
+                <Box width={theme.spacing(28)} height="100%" display="flex" flexDirection="column" >
+                    {avatarBox()}
+
+                    <Divider />
+
+                    <Box flex={1}>
+                        <List component="nav">
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <Icon>home</Icon>
+                                </ListItemIcon>
+                                <ListItemText primary="Página inicial" />
+                            </ListItemButton>
+                        </List>
+                    </Box>
+                </Box>
+            </Drawer>
+
+            <Box height="100vh" marginLeft={theme.spacing(28)}>
+                {children}
+            </Box>
+
+
+
+        </>
+    )
+
+
+}
