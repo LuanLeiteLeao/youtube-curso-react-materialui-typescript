f0bbda0aac3aea260f04a75cfbdf60ecabf0e5f5
Luan Leite Leão <leaoluanleite@gmail.com>
2024-07-21 22:18:34 -0300

    reft: criando funcoes para responsividade

--- src/shared/layouts/LayoutBaseDaPagina.tsx
+++ src/shared/layouts/LayoutBaseDaPagina.tsx
@@ -15,6 +15,14 @@ export const LayoutBaseDaPagina: React.FC<IProps> = (props) => {
     const isMdDown = useMediaQuery(theme.breakpoints.down("md"))
     const { toggleDrawerOpen } = useDrawerContext()
 
+    function getResponsiveTypographyVariant() {
+        return isSmDown ? "h5" : isMdDown ? "h4" : "h3"
+    }
+
+    function getResponsiveBoxHeight() {
+        return theme.spacing(isSmDown ? 6 : isMdDown ? 8 : 12)
+    }
+
     function botaoDeMenuResponsivo() {
         return isSmDown && (
             <IconButton onClick={toggleDrawerOpen}>
@@ -26,9 +34,9 @@ export const LayoutBaseDaPagina: React.FC<IProps> = (props) => {
     return (
         <Box height="100%" display="flex" flexDirection="column" gap={1}>
 
-            <Box padding={1} display="flex" alignItems="center" height={theme.spacing(isSmDown ? 6 : isMdDown ? 8 : 12)}>
+            <Box padding={1} display="flex" alignItems="center" height={getResponsiveBoxHeight()}>
                 {botaoDeMenuResponsivo()}
-                <Typography overflow="hidden" whiteSpace="nowrap" textOverflow="ellipsis" variant={isSmDown ? "h5" : isMdDown ? "h4" : "h3"}>
+                <Typography overflow="hidden" whiteSpace="nowrap" textOverflow="ellipsis" variant={getResponsiveTypographyVariant()}>
                     {props.titulo}
                 </Typography>
             </Box>
@@ -47,6 +55,10 @@ export const LayoutBaseDaPagina: React.FC<IProps> = (props) => {
 
         </Box>
     )
+
+
+
+
 
 
 }
